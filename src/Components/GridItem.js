0e60685d30e91fb7aa7cf1ff68d16e2899ec2e18
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-10-07 21:56:15 -0600

    Estilos terminados

--- src/Components/GridItem.js
+++ src/Components/GridItem.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const GridItem = ({ url, title }) => {
     // https://animate.style/
     return (
-        <img className={"animate__animated animate__backInUp"} src={url} alt={title} />
+        <img className={"animate__animated animate__backInUp card"} src={url} alt={title} />
     );
 }
